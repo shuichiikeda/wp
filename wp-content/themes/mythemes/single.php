6f75326a9cfdd34beb7f0c2828721a7b0b48b293
Shuichi Ikeda <s22d.da@gmail.com>
2018-08-08 23:07:35 +0900

    元に戻す

--- wp-content/themes/mythemes/single.php
+++ wp-content/themes/mythemes/single.php
@@ -3,7 +3,6 @@
 <div class="content section-inner">
 											        
 	<?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
-    <p><?php single_cat_title(); ?>カテゴリー記事の一覧です</p>
 		<div id="post-<?php the_ID(); ?>" <?php post_class( 'single single-post' ); ?>>
 			
 			<div class="post-container">
