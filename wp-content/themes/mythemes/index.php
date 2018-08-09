3cd21758dcf7703dc89587004e78d22b6c782b2f
Shuichi Ikeda <s22d.da@gmail.com>
2018-08-10 00:00:19 +0900

    edit index.php

--- wp-content/themes/mythemes/index.php
+++ wp-content/themes/mythemes/index.php
@@ -1,7 +1,19 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: shuichi
- * Date: 2018/08/09
- * Time: 23:48
- */
+<?php get_header(); ?>
+
+<main>
+
+    <?php if(have_posts()): while(have_posts()):the_post(); ?>
+
+        <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+
+        <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y.m.d'); ?></time>
+        <p><?php the_category(', '); ?></p>
+        <p><?php the_content('Read more'); ?></p>
+
+    <?php endwhile; endif; ?>
+
+</main>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>​
