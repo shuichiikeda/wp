0d691b20fccd137d813e3fa83fc2549fdaa5616b
Shuichi Ikeda <s22d.da@gmail.com>
2018-08-11 10:50:08 +0900

    facebookページテスト

--- wp-content/themes/mythemes/footer.php
+++ wp-content/themes/mythemes/footer.php
@@ -5,8 +5,18 @@
 	<div class="clear"></div>
 	
 </div><!-- .credits -->
-
+<div class="fb-page" data-href="https://www.facebook.com/Web%E3%82%AF%E3%83%AA%E3%82%A8%E3%82%A4%E3%82%BF%E3%83%BC%E3%81%AF%E3%82%84%E3%82%81%E3%81%A8%E3%81%91-2128537574025382/?notif_id=1533824063642053&amp;notif_t=page_admin&amp;ref=notif" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"></div>
 <?php wp_footer(); ?>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.1';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+</script>
+
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <!-- kaigai website -->
 <ins class="adsbygoogle"
