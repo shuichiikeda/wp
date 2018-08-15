f7317e7d0b0f0ef5cfc8ee15e94220a83e365022
Shuichi Ikeda <s22d.da@gmail.com>
2018-08-15 23:19:22 +0900

    targetblank facebook

--- wp-content/themes/mythemes/footer.php
+++ wp-content/themes/mythemes/footer.php
@@ -3,9 +3,8 @@
 	<p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a></p>
 
 	<div class="clear"></div>
-	
+  <div class="fb-page" data-href="https://www.facebook.com/Web%E3%82%AF%E3%83%AA%E3%82%A8%E3%82%A4%E3%82%BF%E3%83%BC%E3%81%AF%E3%82%84%E3%82%81%E3%81%A8%E3%81%91-2128537574025382/" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Web%E3%82%AF%E3%83%AA%E3%82%A8%E3%82%A4%E3%82%BF%E3%83%BC%E3%81%AF%E3%82%84%E3%82%81%E3%81%A8%E3%81%91-2128537574025382/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Web%E3%82%AF%E3%83%AA%E3%82%A8%E3%82%A4%E3%82%BF%E3%83%BC%E3%81%AF%E3%82%84%E3%82%81%E3%81%A8%E3%81%91-2128537574025382/">Webクリエイターはやめとけ</a></blockquote></div>
 </div><!-- .credits -->
-<div class="fb-page" data-href="https://www.facebook.com/Web%E3%82%AF%E3%83%AA%E3%82%A8%E3%82%A4%E3%82%BF%E3%83%BC%E3%81%AF%E3%82%84%E3%82%81%E3%81%A8%E3%81%91-2128537574025382/" data-tabs="timeline" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Web%E3%82%AF%E3%83%AA%E3%82%A8%E3%82%A4%E3%82%BF%E3%83%BC%E3%81%AF%E3%82%84%E3%82%81%E3%81%A8%E3%81%91-2128537574025382/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Web%E3%82%AF%E3%83%AA%E3%82%A8%E3%82%A4%E3%82%BF%E3%83%BC%E3%81%AF%E3%82%84%E3%82%81%E3%81%A8%E3%81%91-2128537574025382/">Webクリエイターはやめとけ</a></blockquote></div>
 <?php wp_footer(); ?>
 
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
