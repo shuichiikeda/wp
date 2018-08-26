a2bc31371b347eea3f7cde0213462e2893320d54
Shuichi Ikeda <s22d.da@gmail.com>
2018-08-26 22:06:44 +0900

    edit function

--- wp-content/themes/dazzling/functions.php
+++ wp-content/themes/dazzling/functions.php
@@ -331,3 +331,67 @@ function of_get_option( $name, $default = false ) {
   return $default;
 }
 endif;
+
+//---------------------------------------//
+// 内部リンクのブログカード化（ショートコード）
+// ここから
+//---------------------------------------//
+
+// 記事IDを指定して抜粋文を取得する
+function ltl_get_the_excerpt($post_id){
+    $post = get_post($post_id);
+    $output = mb_substr(str_replace(array("\r\n", "\r", "\n", " ", "　", "&nbsp;"), '', strip_tags(apply_filters( 'the_content', $post -> post_content ))), 0, 150) . "...";
+    return $output;
+}
+
+//内部リンクをはてなカード風にするショートコード
+function nlink_scode($atts) {
+    extract(shortcode_atts(array(
+        'url'=>"",
+        'title'=>"",
+        'excerpt'=>""
+    ),$atts));
+
+    $id = url_to_postid($url);//URLから投稿IDを取得
+
+    $img_width ="90";//画像サイズの幅指定
+    $img_height = "90";//画像サイズの高さ指定
+    $no_image = 'noimageに指定したい画像があればここにパス';//アイキャッチ画像がない場合の画像を指定
+
+    //タイトルを取得
+    if(empty($title)){
+        $title = esc_html(get_the_title($id));
+    }
+    //抜粋文を取得
+    if(empty($excerpt)){
+        $excerpt = esc_html(ltl_get_the_excerpt($id));
+    }
+
+    //アイキャッチ画像を取得
+    if(has_post_thumbnail($id)) {
+        $img = wp_get_attachment_image_src(get_post_thumbnail_id($id),array($img_width,$img_height));
+        $img_tag = "<img src='" . $img[0] . "' alt='{$title}' width=" . $img[1] . " height=" . $img[2] . " />";
+    }else{
+        $img_tag ='<img src="'.$no_image.'" alt="" width="'.$img_width.'" height="'.$img_height.'" />';
+    }
+
+    $nlink .='
+<div class="blog-card">
+  <a href="'. $url .'" target="_blank">
+      <div class="blog-card-thumbnail">'. $img_tag .'</div>
+      <div class="blog-card-content">
+          <div class="blog-card-title">'. $title .' </div>
+          <div class="blog-card-excerpt">'. $excerpt .'</div>
+      </div>
+      <div class="clear"></div>
+  </a>
+</div>';
+
+    return $nlink;
+}
+
+add_shortcode("nlink", "nlink_scode");
+//---------------------------------------//
+// ここまで
+// 内部リンクのブログカード化（ショートコード）
+//---------------------------------------//
