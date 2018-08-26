3ee5bc5f3824eec085af9c59dbea890f77b6c94d
Shuichi Ikeda <s22d.da@gmail.com>
2018-08-26 15:40:21 +0900

    edit css

--- wp-content/themes/dazzling/content.php
+++ wp-content/themes/dazzling/content.php
@@ -5,7 +5,11 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header page-header">
+  <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
+      <?php the_post_thumbnail( 'dazzling-featured', array( 'class' => 'thumbnail col-sm-6' )); ?>
+  </a>
+
+  <header class="entry-header page-header">
 
 		<h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 
@@ -40,19 +44,6 @@
 	</div><!-- .entry-summary -->
 	<?php else : ?>
 	<div class="entry-content">
-
-		<?php if ( has_post_thumbnail()) : ?>
-		<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
-		 	<?php the_post_thumbnail( 'dazzling-featured', array( 'class' => 'thumbnail col-sm-6' )); ?>
-		</a>
-		<div class="col-sm-6">
-			<?php the_excerpt(); ?>
-		</div>
-		<?php else : ?>
-			<?php the_excerpt(); ?>
-		<?php endif; ?>
-		<p><a class="btn btn-default read-more" href="<?php the_permalink(); ?>"><?php _e( 'Continue reading', 'dazzling' ); ?> <i class="fa fa-chevron-right"></i></a></p>
-
 		<a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"></a>
 
 
