76f76f7c8d33edf6f57883b3d8347c44a1908fe4
Shuichi Ikeda <s22d.da@gmail.com>
2018-08-26 22:40:24 +0900

    test

--- wp-content/themes/dazzling/content.php
+++ wp-content/themes/dazzling/content.php
@@ -26,7 +26,6 @@
 				if ( $categories_list && dazzling_categorized_blog() ) :
 			?>
 			<span class="cat-links"><i class="fa fa-folder-open-o"></i>
-        <?php var_dump($categories_list) ?>
 				<?php printf( __( ' %1$s', 'dazzling' ), $categories_list[0] ); ?>
 			</span>
 			<?php endif; // End if categories ?>
